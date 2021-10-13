891ee348abe13561c38ddefb66422acd6a52f7bd
Junseok Lee <boris0730@gmail.com>
2021-10-13 19:00:55 +0900

    일기장 작성 저장 구현

---
+++ IOS_Diary/IOS_Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  IOS_Diary
+//
+//  Created by Junseok Lee on 2021/10/13.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
